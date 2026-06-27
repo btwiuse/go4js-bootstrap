5186ba17146f9432ae59d928757e6e0a5f3494fa
Broken Pipe <navigaid@gmail.com>
2026-06-27 17:16:27 +0800

    use pure src for target

--- build.sh
+++ build.sh
@@ -2,18 +2,22 @@
   curl -LO https://github.com/justwasm/go4js/releases/download/go1.27.0-go4js.1/go1.27.0-go4js.1.js-wasm.min.tar.gz
 }
 
+[[ -f go1.27.0-go4js.1.js-wasm.src.min.tar.gz ]] || {
+  curl -LO https://github.com/justwasm/go4js/releases/download/go1.27.0-go4js.1/go1.27.0-go4js.1.js-wasm.src.min.tar.gz
+}
+
 [[ -f main.wasm ]] || {
   curl -sL https://justwasm.github.io/hackpad/wasm/main.wasm > main.wasm
 }
 
-src=goroot
-dst=work
+boot=goroot
+target=work
 
-mkdir -p $src $dst
-rm -rf $src $dst
-
-tar xf go1.27.0-go4js.1.js-wasm.min.tar.gz
-mv go $dst
+mkdir -p $boot $target
+rm -rf $boot $target
 
 tar xf go1.27.0-go4js.1.js-wasm.min.tar.gz
-mv go $src
+mv go $boot
+
+tar xf go1.27.0-go4js.1.js-wasm.src.min.tar.gz
+mv go $target
